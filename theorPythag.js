0027545f594cc0354fd85521262818856b9ac612
informathemusic <39065949+informathemusic@users.noreply.github.com>
2020-06-05 12:39:21 +0200

    Ai ajouté la rcp

--- theorPythag.js
+++ theorPythag.js
@@ -19,21 +19,49 @@ while(!T.some(v=>v===R)){
 	R=R.toUpperCase()
 };
 let points=T.filter(v=>v!=R)
-let answer=await ask('Theoreme ou reciproque du theoreme de pythagore? [R/t]')
-if(answer.toLowerCase()=='t'){
+if((await ask('Rcp? [o/N]')).toLowerCase()=='o'){
+let c=[await ask(`${points[0]}${R}=`), await ask(`${points[1]}${R}=`), await ask(`${points[0]}${points[1]}=`)]
+await ask(`Resultat:
+
+##### a.
+
+${points[0]}${points[1]} pourrait être le coté de l'hypoténuse car il est plus grand que les deux autre cotés séparément mais est plus petit que la somme des deux autres cotés
+
+##### b.
+
+Dans le triangle MNP, le plus long coté est NP
+
+Donc on calcule séparément:
+
+${points[0]}${points[1]}²=${c[2]}²
+
+${points[0]}${points[1]}²=${Math.round(c[2]**2*1e6)/1e6}
+
+${points[0]}${R}²+${points[1]}${R}²=(${c[0]}²)+(${c[1]}²)
+
+${points[0]}${R}²+${points[1]}${R}²=${Math.round(c[0]**2*1e6)/1e6}+${Math.round(c[1]**2*1e6)/1e6}
+
+${points[0]}${R}²+${points[1]}${R}²${Math.round(c[2]**2*1e6)/1e6==(Math.round(c[0]**2*1e6)/1e6+Math.round(c[1]**2*1e6)/1e6)?'=':'≠'}${points[0]}${points[1]}²
+
+##### c.
+
+${T.join('')} ${Math.round(c[2]**2*1e6)/1e6!=(Math.round(c[0]**2*1e6)/1e6+Math.round(c[1]**2*1e6)/1e6)?'n\'':''}est ${Math.round(c[2]**2*1e6)/1e6==(Math.round(c[0]**2*1e6)/1e6+Math.round(c[1]**2*1e6)/1e6)?'':'pas'} rectangle.`)
+process.exit()
+}else{
+let answer=await ask('Calculer l\'hypothénuse ou un autre coté? [H/a]')
+if(!answer.toLowerCase()=='a'){
   let a=await ask(`${points[0]}${R}=`)
   let a2=await ask(`${points[1]}${R}=`)
   await ask(`Resultat: Je sais que dans le triangle ${T.join('')} rectangle en ${R}, {${points[0]}${R}=${a} et ${points[1]}${R}=${a2}}.
-
 Or, selon le theoreme de pythagore, la somme des deux cotés au carré est égale au carré de l'hypothénuse.
 \`\`\`markdown
 Donc ${points[0]}${points[1]}=√(${points[0]}${R}²+${points[0]}${R}²)
      ${points[0]}${points[1]}=√(${a}²+${a2}²)
-     ${points[0]}${points[1]}=√(${a**2}+${a2**2})
-     ${points[0]}${points[1]}=${(a**2+a2**2)**.5}${u}
+     ${points[0]}${points[1]}=√(${Math.round(a**2*1e6)/1e6}+${Math.round(a2**2*1e6)/1e6})
+     ${points[0]}${points[1]}=${(Math.round(a**2*1e6)/1e6+Math.round(a2**2*1e6)/1e6)**.5}${u}
 \`\`\``)
   process.exit()
-}else if(answer.toLowerCase()=='r'){
+}else{
   let ptCorr='';
   while(!(ptCorr.length==1&&ptCorr!=R)){
     ptCorr=await ask(`Point du coté a soustraire (Indice: Pas ${R}):`)
@@ -43,17 +71,14 @@ Donc ${points[0]}${points[1]}=√(${points[0]}${R}²+${points[0]}${R}²)
   let a=await ask(`${points[0]}${points[1]}=`)
   let a2=await ask(`${ptCorr}${R}=`)
   await ask(`Resultat: Je sais que dans le triangle ${T.join('')} rectangle en ${R}, {${points[0]}${points[1]}=${a} et ${ptCorr}${R}=${a2}}.
-
-Or, selon la réciproque du theoreme de pythagore, pour calculer un coté dans un triangle rctangle qui n'est pas l'hypothénuse, il faut soustraire l'autre coté au carré a l'hypothénuse.
+Or, selon le theoreme de pythagore, pour calculer un coté dans un triangle rctangle qui n'est pas l'hypothénuse, il faut soustraire l'autre coté au carré a l'hypothénuse.
 \`\`\`markdown
 Donc ${ptMiss}${R}=√(${points[0]}${points[1]}²-${ptCorr}${R}²)
      ${ptMiss}${R}=√(${a}²-${a2}²)
-     ${ptMiss}${R}=√(${a**2}-${a2**2})
-     ${ptMiss}${R}=${(a**2-a2**2)**.5}${u}
+     ${ptMiss}${R}=√(${Math.round(a**2*1e6)/1e6}-${Math.round(a2**2*1e6)/1e6})
+     ${ptMiss}${R}=${(Math.round(a**2*1e6)/1e6-Math.round(a2**2*1e6)/1e6)**.5}${u}
 \`\`\``)
   process.exit()
-}else{
-  await ask('Reponse incompréhensible')
-  process.exit()
+}
 }
 })()
